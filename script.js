e41e5faec670687387552e674f6bda55644b6277
Arigithubs <arimail@duck.com>
2023-12-27 20:49:14 -0500

    Add calming environment and adaptive puzzles

--- script.js
+++ script.js
@@ -1,19 +1,55 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // JavaScript spells for initializing the game background and interactions
+const container = document.getElementById("gameContainer");
 
-    const container = document.getElementById("gameContainer");
+// Core Interactions: Creating serene environment and adaptive puzzles
+createCalmingEnvironment();
+createAdaptivePuzzles();
 
-    // Core Interactions: Creating serene environment and adaptive puzzles
-    createCalmingEnvironment();
-    createAdaptivePuzzles();
+function createCalmingEnvironment() {
+    // Enchant with soothing visuals and ambient sounds
+    createParticles();
+    createCelestialElements();
 
-    function createCalmingEnvironment() {
-        // Enchant with soothing visuals and ambient sounds
-        // Code for creating a serene environment goes here
+    function createParticles() {
+        // Enchant with gentle particles floating across the serene environment
+        for (let i = 0; i < 10; i++) {
+            createParticle();
+        }
+
+        function createParticle() {
+            const particle = document.createElement("div");
+            particle.className = "particle";
+            particle.style.top = `${Math.random() * 100}vh`;
+            particle.style.left = `${Math.random() * 100}vw`;
+            container.appendChild(particle);
+        }
     }
 
-    function createAdaptivePuzzles() {
-        // Enchant with adaptive puzzles that start simple and increase in complexity
-        // Code for creating adaptive puzzles goes here
+    function createCelestialElements() {
+        // Enchant with star constellations and gentle star trails
+        for (let i = 0; i < 5; i++) {
+            createConstellation();
+            createStarTrail();
+        }
+
+        function createConstellation() {
+            const constellation = document.createElement("div");
+            constellation.className = "constellation";
+            constellation.style.top = `${Math.random() * 100}vh`;
+            constellation.style.left = `${Math.random() * 100}vw`;
+            container.appendChild(constellation);
+        }
+
+        function createStarTrail() {
+            const starTrail = document.createElement("div");
+            starTrail.className = "star-trail";
+            starTrail.style.top = `${Math.random() * 100}vh`;
+            starTrail.style.left = `${Math.random() * 100}vw`;
+            container.appendChild(starTrail);
+        }
     }
-});
+}
+
+function createAdaptivePuzzles() {
+    // Enchant with adaptive puzzles that start simple and increase in complexity
+    // Code for creating adaptive puzzles goes here
+}
