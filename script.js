24d2cc0a01178684480fe483676f5efd6086fb8a
Arigithubs <arimail@duck.com>
2023-12-27 20:34:55 -0500

    Add celestial elements dynamically

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
+// script.js
+
 document.addEventListener("DOMContentLoaded", function () {
     // JavaScript spells for initializing the game background
+
+    const container = document.getElementById("gameContainer");
+
+    // Add celestial elements (constellations, star trails, particles) dynamically
+    for (let i = 0; i < 10; i++) {
+        createCelestialElement("constellation");
+        createCelestialElement("star-trail");
+        createParticle();
+    }
+
+    function createCelestialElement(className) {
+        const element = document.createElement("div");
+        element.className = className;
+        element.style.top = `${Math.random() * 100}vh`;
+        element.style.left = `${Math.random() * 100}vw`;
+        container.appendChild(element);
+    }
+
+    function createParticle() {
+        const particle = document.createElement("div");
+        particle.className = "particle";
+        particle.style.top = `${Math.random() * 100}vh`;
+        particle.style.left = `${Math.random() * 100}vw`;
+        container.appendChild(particle);
+    }
 });
